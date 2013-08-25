791b8c033c7eb9d746a1500e3590c84ac764a4d2
sarabpreet <sarabpreetsingh23@gmail.com>
2013-08-26 00:04:59 +0530

    Adding insert post and update post
    created two new functions and modified the earlier database connection
    function, now database connection does'nt gives any message, instead it
    is now a function which returns a boolean type.
    
    Also added insert_post function and update_post which excepts an array
    for body. it also returns a boolean type.

--- functions/db_functions.php
+++ functions/db_functions.php
@@ -2,31 +2,33 @@
 
 function database_connect() {
 /*
-
-mysql connect
-
-*/
+*
+* @mysql connects and selects the main database.
+* @function name -> database_connect() 
+* @parameters -> none
+* @returns a boolean and resource 
+* /
 
 $connect =mysql_connect('localhost','root',''); // connecting the database from localhost
 
 if(!$connect) {
 
-die('ERROR, connecting to database server'.mysql_error());
+return false;
 
 }
  
  /* selecting the database */
 if(!mysql_select_db('umawings',$connect)) {
 
-	die("Eror, connecting to the selected database ".mysql_error()); 
+	return false;
 }
-
-
+return $connect;
 
 } // function close
 
 
 
-database_connect();
+database_connect(); //calling database_connect
+
 
 ?>
--- functions/query.php
+++ functions/query.php
@@ -15,23 +15,22 @@ echo $row[2];
 
 function insert() {
 
-$query ='insert into wng_post set wng_title="some title goes here",
-							  wng_body="some random post goes here....",
-							  wng_created_at=now(),
-							  wng_user_id=1
-							  ';
 
 
-$result=mysql_query($query);
-if(!$result){
-echo mysql_error();
-}
-else {
-echo "added that row";
-}
+		$query ='insert into wng_post set wng_title="some title goes here",
+									  wng_body="some random post goes here....",
+									  wng_created_at=now(),
+									  wng_user_id=1
+									  ';
+		$result=mysql_query($query);
+		if(!$result){
+		echo mysql_error();
+		}
+		else {
+		echo "added that row";
+		}
 
-
-} 
+}  //inserts ends...
 
 
 function update() {
@@ -79,6 +78,86 @@ echo "Row Deleted";
 //insert();
 //update();
 //delete();
+
+//---------------------now some serious functions------------------------------------//
+
+
+function create_post($params){
+
+
+/*
+
+*creates a post 
+*@param type : array name: $params
+*@returns : bool
+
+*/
+
+			$connection = database_connect();
+			$query=sprintf("insert into wng_post 
+				set wng_title='%s',wng_body='%s',wng_created_at=NOW(),
+				wng_user_id='%s'",
+				mysql_real_escape_string($params['wng_title']),
+				mysql_real_escape_string($params['wng_body']),
+				mysql_real_escape_string($params['wng_user_id']));
+
+
+					$result=mysql_query($query);
+					if(!$result) {
+						return false;
+					}
+					else {
+					return true;
+					}
+
+}
+
+/* create_post(array('wng_title'=>'this is the tittle from the function',
+				  'wng_body'=>'This is the body',
+				  'wng_user_id'=>1));
+*/
+
+
+
+function update_post($params){
+
+
+				/*
+
+				*updates a post 
+				*@param type : array name: $params
+				*@returns : bool
+
+				*/
+
+			$connection = database_connect();
+			$query=sprintf("update wng_post 
+				set wng_title='%s',wng_body='%s',wng_created_at=NOW(),
+				wng_user_id='%s'
+				where wng_id=%s",
+				mysql_real_escape_string($params['wng_title']),
+				mysql_real_escape_string($params['wng_body']),
+				mysql_real_escape_string($params['wng_user_id']),
+				mysql_real_escape_string($params['wng_id'])
+				);
+
+
+					$result=mysql_query($query);
+					if(!$result) {
+						return false;
+					}
+					else {
+					return true;
+					}
+
+
+
+}
+
+update_post(array('wng_title'=>'this is the updated post',
+				  'wng_body'=>'This is the updated body',
+				  'wng_user_id'=>1,
+				  'wng_id'=>177));
 
 
 ?>
