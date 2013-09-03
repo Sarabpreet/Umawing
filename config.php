b0cfbd8452841c7d88a95808a6208bbf80ecf218
sarabpreet <sarabpreetsingh23@gmail.com>
2013-09-04 01:10:13 +0530

    Converting Routes to Functions
    "A COMMIT FULL OF BUG", Also introducing config.php

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php 
+
+//routes url to controller and view 
+
+$routes=array(
+
+  array('url'=>'/^posts\/(?P<id>\d+)$/','controller'=>'posts','view'=>'show'),
+  array('url'=>'/^posts\/(?P<id>\d+)\/edit$/','controller'=>'posts','view'=>'show')
+				
+			);
+
+
+
+//$_SERVER['DOCUMENT_ROOT'].'umawings/controllers'
+
+define('SERVER_ROOT',$_SERVER['DOCUMENT_ROOT']);
+
+//Directory Structure 
+
+define('DS','/');
+
+
+//Application Directory 
+
+define('APP_ROOT','umawings');
+
+//MVC paths 
+
+define('MODEL_PATH',SERVER_ROOT.APP_ROOT.DS.'models'.DS);
+define('VIEW_PATH',SERVER_ROOT.APP_ROOT.DS.'view'.DS);
+define('CONTROLLER_PATH',SERVER_ROOT.APP_ROOT.DS.'controller'.DS);
+
+/*
+echo MODEL_PATH."<br>";
+echo VIEW_PATH."<br>";
+echo CONTROLLER_PATH;
+*/
+?>
--- index.php
+++ index.php
@@ -2,50 +2,11 @@
 
 <?php include "functions/functions.php";
 
-	$url=$_SERVER['REQUEST_URI'];
 
-	$url=str_replace('/umawings/','',$url);
 
 
 	
 	//posts/2/edit
-
-
-
-
-$routes=array(
-
-  array('url'=>'/^posts\/(?P<id>\d+)$/','controller'=>'posts','view'=>'show'),
-  array('url'=>'/^posts\/(?P<id>\d+)\/edit$/','controller'=>'posts','view'=>'show')
-				
-			);
-
-$params=array();
-
-foreach ($routes as $urls => $route) {
-
-	echo "looking for match<br/>";
-
-	if(preg_match($route['url'],$url,$matches)) {
-
-	$params=array_merge($params,$matches);
-	$route_match=true;
-
-	include($_SERVER['DOCUMENT_ROOT'].'umawings/controller/'.$route['controller'].'.php');
-
-
-	//print('controller: '.$route['controller'].'<br>'.'view :'.$route['view']. ' id :'.$params['id']);
-	//echo $url;
-
-	break;
-	}
-	else {
-
-		echo('no route found');
-	}
-}
-	
-
 
 //preg_match('/^posts\/(?P<id>\d+)\/edit$/',$url,$matches);
 
@@ -53,6 +14,56 @@ foreach ($routes as $urls => $route) {
 //include($_SERVER['DOCUMENT_ROOT'].'umawings/controller/'.$route['controller'].'.php');
 
 
-	include($_SERVER['DOCUMENT_ROOT'].'umawings/frontend/home.php');
+
+
+include('config.php');
+
+
+function dispatcher($routes) {
+
+
+//requested url
+
+	$url=$_SERVER['REQUEST_URI'];
+
+//removes application root from url
+	$url=str_replace('/'.APP_ROOT.'/','',$url);
+
+//holds the named captures
+	$params=array();
+
+//becomes true if $route['url'] matches
+
+	$route_math=false;
+
+//loops over routes, looking for a match ... 
+foreach ($routes as $urls => $route) {
+
+
+//if match found, appends $matches to $params
+//sets route_match to true and exits the loop.
+
+			if(preg_match($route['url'],$url,$matches)) {
+			$params=array_merge($params,$matches);
+			$route_match=true;
+			include($_SERVER['DOCUMENT_ROOT'].'umawings/controller/'.$route['controller'].'.php');
+			break;
+			}
+		}
+
+
+	if(!route_match) {
+			exit('no route found');
+	}
+
+//include($_SERVER['DOCUMENT_ROOT'].'umawings/controller/'.$route['controller'].'.php');
+
+}
+
+
+
+dispatcher();
+
+	//include($_SERVER['DOCUMENT_ROOT'].'umawings/frontend/home.php');
 
 ?>
