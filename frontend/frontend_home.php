9b095bdf6998cb43592bd80a1c8428dce026d37a
sarabpreet <sarabpreetsingh23@gmail.com>
2013-10-05 00:44:20 +0530

    html structure for public

---
+++ frontend/frontend_home.php
@@ -0,0 +1,144 @@
+<?php 
+$path='frontend/';
+
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Your Page Title Here :)</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+
+
+<div class="margin"></div>
+
+<div class="row">
+	
+<header class="container">
+	
+
+<h1 class="logo two column">Uma wings</h1>
+<nav>
+	
+
+	<ul class="fourteen column">
+		<li><a href="#">About</a></li>
+		<li><a href="#">How it works</a></li>
+		<li><a href="#">Sign up</a></li>
+	</ul>
+</nav>
+
+<div class="one-third columns">
+	
+	<h2>Introducing something cool</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, impedit, magni sunt aliquid cumque ea nemo optio consequatur cupiditate temporibus minus quasi blanditiis harum voluptatum tenetur laboriosam soluta unde iusto?</p>
+
+</div>
+
+<div class="one-third columns">
+	<strong>been here before?:login</strong>
+	<input type="text">
+	<input type="password">
+	<input type="submit">
+</div>
+
+
+<div class="one-third columns">
+	<strong>been here before?:login</strong>
+	<input type="text">
+	<input type="email">
+	<input type="password">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, explicabo.</p>
+	<input type="submit">
+</div>
+</header>
+
+</div>
+<!-- row ends here...-->
+
+<div class="row">
+	
+
+<section class="container">
+	
+<div class="one-third column">
+	
+<h3>mobile friendly</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, voluptatibus, quod obcaecati nulla pariatur ducimus qui magni commodi.</p>
+</div>
+
+
+<div class="one-third column">
+	
+<h3>fast and cute</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, voluptatibus, quod obcaecati nulla pariatur ducimus qui magni commodi.</p>
+</div>
+
+<div class="one-third column">
+	
+<h3>open source</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, voluptatibus, quod obcaecati nulla pariatur ducimus qui magni commodi.</p>
+</div>
+
+</section>
+</div>
+<!-- row ends here...-->
+
+
+<div class="row">
+	
+
+	<footer class="container">
+		
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Login</a></li>
+			<li><a href="#">Career</a></li>
+			<li><a href="#">Developers</a></li>
+			<li><a href="#">Statistics</a></li>
+		</ul>
+		<p>Lorem ipsum dolor sit ametaesentium enim alias non in perspiciatis omnis dolor.</p>
+	</footer>
+</div>
+
+<!-- row ends here...-->
+
+
+</body>
+</html>
+
